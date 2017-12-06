7b1e43eff67ffcd4313f0e1a3ee10b609f730bef
hajduboti <boto0018@stud.kea.dk>
2017-12-06 13:32:32 +0100

    mainpage v0.1

--- src/main/java/ck/ordering/Controller/MenuController.java
+++ src/main/java/ck/ordering/Controller/MenuController.java
@@ -1,7 +1,7 @@
 package ck.ordering.Controller;
 
 
-import ck.ordering.Entities.Menu;
+import ck.ordering.Entities.Item;
 import ck.ordering.Repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Map;
 
 @RestController
 public class MenuController {
@@ -18,26 +17,32 @@ public class MenuController {
     @Autowired
     MenuRepository mr;
 
+    @GetMapping("/")
+    public ModelAndView homeview(){
+        ModelAndView mv = new ModelAndView("mainpage");
+        return mv;
+    }
+
 
     @RequestMapping("/menu")
-    public Collection<Menu> getMenu(){
+    public Collection<Item> getMenu(){
 
         return Arrays.asList(
-                new Menu(1,"bawl",90.00),
-                new Menu(2,"bawl",90.00),
-                new Menu(3,"bawl",90.00)
+                new Item(1,"bawl",90.00),
+                new Item(2,"bawl",90.00),
+                new Item(3,"bawl",90.00)
         );
     }
 
     @RequestMapping("/checkout/{one}/{two}/{three}/{four}/{five}")
-    public  Collection<Menu>  getSelectedMenu(
+    public  Collection<Item>  getSelectedMenu(
             @PathVariable int one,
             @PathVariable int two,
             @PathVariable int three,
             @PathVariable int four,
             @PathVariable int five)
     {
-        ArrayList<Menu> basket = new ArrayList();
+        ArrayList<Item> basket = new ArrayList();
 
         if(one > 0){
             for(int i = 0; i < one;i++) {
